ea72d2a8bcc98b2ef46b0322247a98e0c25f5f3f
auliamey <auliaannandya@gmail.com>
2024-11-26 20:29:40 +0700

    fix: adjust table sql

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -12,6 +12,17 @@ export const UserModel = {
     return result.rows[0];
   },
 
+  async createUser(name, age, email, hashedPassword) {
+    const query = `
+      INSERT INTO users (name, age, company_id, email, password)
+      VALUES ($1, $2, NULL, $3, $4)
+      RETURNING id, name, age, email, created_at;
+    `;
+    const values = [name, age, email, hashedPassword];
+    const result = await client.query(query, values);
+    return result.rows[0];
+  },
+
   async findByEmail(email) {
     const query = "SELECT * FROM users WHERE email = $1;";
     const result = await client.query(query, [email]);
@@ -19,7 +30,7 @@ export const UserModel = {
   },
 
   async getAllUsers() {
-    const query = "SELECT id, name, email, created_at FROM users;";
+    const query = "SELECT id, name, email, age FROM users;";
     const result = await client.query(query);
     return result.rows;
   }
