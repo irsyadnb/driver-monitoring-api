4960540f96b4fc2a3e1823a2bdb3e74c7af528d6
auliamey <auliaannandya@gmail.com>
2024-12-09 20:20:53 +0700

    add: update profile

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -52,5 +52,28 @@ export const AuthController = {
       console.error(error);
       return res.status(500).json({ error: "Internal server error" });
     }
+  },
+
+  async updateProfile(req, res) {
+    try {
+      const { id } = req.params; 
+      const { name, password } = req.body;
+
+      if (!name || !password) {
+        return res.status(400).json({ error: "Name and password are required" });
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      const updatedUser = await UserModel.updateUserProfile(id, name, hashedPassword);
+
+      return res.status(200).json({
+        message: "Profile updated successfully",
+        user: updatedUser,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Internal server error" });
+    }
   }
 };
